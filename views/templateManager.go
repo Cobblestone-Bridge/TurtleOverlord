b41ffbabf70d6d93c9a9eddb243c557f4f9f606f
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2016-03-31 10:49:25 +0200

    Moving over to Goji.

--- views/templateManager.go
+++
@@ -1,7 +0,0 @@
-package views
-
-import (
-  "html/template"
-)
-
-var Templates = template.Must(template.ParseFiles("../src/TurtleOverlord/views/head.html", "../src/TurtleOverlord/views/index.html"))
