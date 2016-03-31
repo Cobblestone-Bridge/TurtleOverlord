b471f92f43d069d8afe64c6357254f1015c269f9
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2016-03-31 18:20:22 +0200

    Started to merge in a dashboard template.

--- controllers/web/index.go
+++ controllers/web/index.go
@@ -13,12 +13,12 @@ import (
 func (controller *Controller) Index(c web.C, r *http.Request) (string, int) {
 	t := controller.GetTemplate(c)
 
-	widgets := helpers.Parse(t, "home", nil)
+	widgets := helpers.Parse(t, "index", nil)
 
 	// With that kind of flags template can "figure out" what route is being rendered
 	c.Env["IsIndex"] = true
 
-	c.Env["Title"] = "Default Project - free Go website project template"
+	c.Env["Title"] = "TurtleOverlord"
 	c.Env["Content"] = template.HTML(widgets)
 
 	return helpers.Parse(t, "main", c.Env), http.StatusOK
